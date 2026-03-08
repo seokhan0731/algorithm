a655df3b20197af513d4a3893a5eae4ee834b42e
Seokhwan Kim <wistin@naver.com>
2026-03-08 15:01:22 +0900

    [Silver IV] Title: 큐, Time: 60 ms, Memory: 35360 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,36 +1,33 @@
+"""
+기존 코드. deque를 통한 큐 구현
+append(left), pop(left)와 인덱싱을 결합하여, 구현
++) 초기에는 append를 하지 않고, 각 command마다 print를 했지만,
+append 후 한번에 출력하는 것이 I/O 관련 병목 현상이나 비용 등에서 유리하기 때문에, append() 후
+한 번에 출력하는 방식으로 변경
+"""
 import sys
 from collections import deque
 
 
-def control_queue(command, queue_val):
+def control_queue(command, queue_val, answer):
     if command.startswith("push"):
         queue_val.append(command.split()[-1])
     elif command == "pop":
-        if queue_val:
-            print(queue_val.popleft())
-        else:
-            print(-1)
+        answer.append(queue_val.popleft() if queue_val else -1)
     elif command == "size":
-        print(len(queue_val))
+        answer.append(len(queue_val))
     elif command == "empty":
-        if queue_val:
-            print(0)
-        else:
-            print(1)
+        answer.append(0 if queue_val else 1)
     elif command == "front":
-        if queue_val:
-            print(queue_val[0])
-        else:
-            print(-1)
+        answer.append(queue_val[0] if queue_val else -1)
     else:
-        if queue_val:
-            print(queue_val[-1])
-        else:
-            print(-1)
+        answer.append(queue_val[-1] if queue_val else -1)
 
 
 # sys.stdin = open("input.txt", "r")
 data = sys.stdin.read().splitlines()[1:]
 queue_val = deque([])
+answer = []
 for line in data:
-    control_queue(line, queue_val)
+    control_queue(line, queue_val, answer)
+print('\n'.join(map(str, answer)))
