41de35cf7fb0ba478fbf48b8eed955028138f682
Seokhwan Kim <wistin@naver.com>
2026-04-07 23:34:29 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 576 ms, Memory: 69380 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -101,8 +101,16 @@ sys.setrecursionlimit(10 ** 8)
 #         component_count += 1
 # print(component_count)
 
-
+# 코드4. 유니온 파인드
 def find_parent(node):
+    """
+    집합의 대표가 누구인지 탐색
+    단순히 return find()꼴이라면, 대표가 바뀌더라도 해당 노드의 값이 반영이 안되기 때문에,
+    저장 후, 해당값을 반환하는 것이 더 효율적
+
+    :param node: 대표를 찾고자하는 노드
+    :return: 집합의 대표값
+    """
     if parent_list[node] == node:
         return node
     parent_list[node] = find_parent(parent_list[node])
@@ -111,6 +119,24 @@ def find_parent(node):
 
 # 작은 노드의 부모 인덱스를 node1이랑 node 2중 누가 갖고 있는지를 알아야되네
 def union(node1, node2):
+    """
+    서로 다른 두 개의 집합에 속하는 정점들을 하나의 집합으로 묶어준다.
+    기존의 bfs, dfs 탐색과는 다르게 union-find 알고리즘은 정점들이 어느 집합에 있는지를 기록하기 떄문에,
+    해당 문제처럼 연결 요소의 개수를 구할 때, 기존 탐색보다 효율적이다.
+    구동 방식은 각 집합의 대표를 수가 가장 작은 노드로 잡는다고 가정했을 떄(계층적 구조 형성 가능),
+    인접한 두 노드(a,b)가 들어왔을 때, 각 노드의 부모(c)->c의 부모... 이런식으로 거슬러 올라가 최종적으로
+    집합의 대표가 누구인지를 기록할 수 있다. 거슬러 올라가기 떄문에 재귀방식으로 구현이 가능하다.
+    각 노드 대표 찾기->대표가 다르다(입력으로 주어진 두 정점은 연결되어 있으니, 같은 집합)->같은 집합으로 묶어준다.
+
+    +) 입력의 선후 관계에 따라, 최종 완성된 집합은 대표들로만 구성되어 있지 않을 수 있다.
+    cuz 이미 구해놓은 정점 대표가, 나중에 대표직을 잃은 경우, 이 시점에 업데이트가 되지 않았을 때
+    그렇더라도, 이미 연결 관계는 정의되어 있으니, 최종적으로 각 정점에 대해 find()를 수행하면 대표값들로만 구성된
+    리스트를 만들 수 있다.
+
+    :param node1: 간선 사이 노드1
+    :param node2: 간선 사이 노드2
+    :return: None(각 노드의 대표값들을 담는 리스트를 직접 수정)
+    """
     global count_val
     node1_parent = find_parent(node1)
     node2_parent = find_parent(node2)
