f04969a31fe901c993d5bd3ce56f9db877101ccc
Seokhwan Kim <wistin@naver.com>
2026-06-27 16:24:10 +0900

    [level 2] Title: 올바른 괄호, Time: 5.19 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,12 @@
+def solution(s):
+    count = 0
+    for char in s:
+        if char == '(':
+            count += 1
+        else:
+            count -= 1
+        if count < 0:
+            return False
+    if count != 0:
+        return False
+    return True
